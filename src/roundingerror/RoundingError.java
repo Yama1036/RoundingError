75fa8ac11c678b757da6a879257dafb6de97ccb1
ayyam8774 <ayyam8774@7JCM6Y1.Student.UGDSB.ED>
2020-02-24 11:15:42 -0500

    Hello

--- src/roundingerror/RoundingError.java
+++ src/roundingerror/RoundingError.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Yama
+ * 24/02/2020
+ * This code shows java give you some rounding error in Math methods.
  */
 
 package roundingerror;
